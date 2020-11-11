a990e51584484df269ba5401f094744dc244507b
OscarXu <15222875592@163.com>
2020-11-11 17:23:18 -0500

    #0001 init
    #0001 main entrance of Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'Oscar\'s first angular js project';
+
+  firstName = "Oscar";
+  lastName = "Xu";
+
 }
